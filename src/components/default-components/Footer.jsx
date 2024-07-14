f7124028a79cc84ffc931c7a02ae445bf6d1d506
atharvyadav96k <atharvyadav96k@gmail.com>
2024-07-14 13:46:31 +0530

    fixed style,class errors  and added favicon to wensite

--- src/components/default-components/Footer.jsx
+++ src/components/default-components/Footer.jsx
@@ -1,38 +1,38 @@
 export default function Footer() {
   return (
     <div>
-      <footer class="text-center text-lg-start bg-body-tertiary text-muted">
-        <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-          <div class="me-5 d-none d-lg-block">
+      <footer className="text-center text-lg-start bg-body-tertiary text-muted">
+        <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+          <div className="me-5 d-none d-lg-block">
           </div>
 
           <div>
-            <a href="" class="me-4 text-reset">
-              <i class="fab fa-facebook-f"></i>
+            <a href="" className="me-4 text-reset">
+              <i className="fab fa-facebook-f"></i>
             </a>
-            <a href="" class="me-4 text-reset">
-              <i class="fab fa-twitter"></i>
+            <a href="" className="me-4 text-reset">
+              <i className="fab fa-twitter"></i>
             </a>
-            <a href="" class="me-4 text-reset">
-              <i class="fab fa-google"></i>
+            <a href="" className="me-4 text-reset">
+              <i className="fab fa-google"></i>
             </a>
-            <a href="" class="me-4 text-reset">
-              <i class="fab fa-instagram"></i>
+            <a href="" className="me-4 text-reset">
+              <i className="fab fa-instagram"></i>
             </a>
-            <a href="" class="me-4 text-reset">
-              <i class="fab fa-linkedin"></i>
+            <a href="" className="me-4 text-reset">
+              <i className="fab fa-linkedin"></i>
             </a>
-            <a href="" class="me-4 text-reset">
-              <i class="fab fa-github"></i>
+            <a href="" className="me-4 text-reset">
+              <i className="fab fa-github"></i>
             </a>
           </div>
         </section>
-        <section class="">
-          <div class="container text-center text-md-start mt-5">
-            <div class="row mt-3">
-              <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                <h6 class="text-uppercase fw-bold mb-4">
-                  <i class="fas fa-gem me-3"></i>Company name
+        <section className="">
+          <div className="container text-center text-md-start mt-5">
+            <div className="row mt-3">
+              <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                <h6 className="text-uppercase fw-bold mb-4">
+                  <i className="fas fa-gem me-3"></i>Company name
                 </h6>
                 <p>
                   Here you can use rows and columns to organize your footer
@@ -40,77 +40,77 @@ export default function Footer() {
                   elit.
                 </p>
               </div>
-              <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                <h6 class="text-uppercase fw-bold mb-4">Products</h6>
+              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 className="text-uppercase fw-bold mb-4">Products</h6>
                 <p>
-                  <a href="#!" class="text-reset">
+                  <a href="#!" className="text-reset">
                     Angular
                   </a>
                 </p>
                 <p>
-                  <a href="#!" class="text-reset">
+                  <a href="#!" className="text-reset">
                     React
                   </a>
                 </p>
                 <p>
-                  <a href="#!" class="text-reset">
+                  <a href="#!" className="text-reset">
                     Vue
                   </a>
                 </p>
                 <p>
-                  <a href="#!" class="text-reset">
+                  <a href="#!" className="text-reset">
                     Laravel
                   </a>
                 </p>
               </div>
-              <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                <h6 class="text-uppercase fw-bold mb-4">Useful links</h6>
+              <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
                 <p>
-                  <a href="#!" class="text-reset">
+                  <a href="#!" className="text-reset">
                     Pricing
                   </a>
                 </p>
                 <p>
-                  <a href="#!" class="text-reset">
+                  <a href="#!" className="text-reset">
                     Settings
                   </a>
                 </p>
                 <p>
-                  <a href="#!" class="text-reset">
+                  <a href="#!" className="text-reset">
                     Orders
                   </a>
                 </p>
                 <p>
-                  <a href="#!" class="text-reset">
+                  <a href="#!" className="text-reset">
                     Help
                   </a>
                 </p>
               </div>
-              <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
                 <p>
-                  <i class="fas fa-home me-3"></i> New York, NY 10012, US
+                  <i className="fas fa-home me-3"></i> New York, NY 10012, US
                 </p>
                 <p>
-                  <i class="fas fa-envelope me-3"></i>
+                  <i className="fas fa-envelope me-3"></i>
                   info@example.com
                 </p>
                 <p>
-                  <i class="fas fa-phone me-3"></i> + 01 234 567 88
+                  <i className="fas fa-phone me-3"></i> + 01 234 567 88
                 </p>
                 <p>
-                  <i class="fas fa-print me-3"></i> + 01 234 567 89
+                  <i className="fas fa-print me-3"></i> + 01 234 567 89
                 </p>
               </div>
             </div>
           </div>
         </section>
         <div
-          class="text-center p-4"
-          style={{backgroundColor: "rgba(0, 0, 0, 0.05);"}}
+          className="text-center p-4"
+          style={{backgroundColor: "rgba(0, 0, 0, 0.05)"}}
         >
           © 2024 Copyright:
-          <a class="text-reset fw-bold" >
+          <a className="text-reset fw-bold" >
           </a>
         </div>
       </footer>
